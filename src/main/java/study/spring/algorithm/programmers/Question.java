f1d6fcb81c12d246a0cbaeb596101b40c0a33276
kimcno3 <kimcno3@gmail.com>
2022-08-17 01:35:15 +0900

    프로그래머스 "순위 검색" 문제 풀이

--- src/main/java/study/spring/algorithm/programmers/Question.java
+++ src/main/java/study/spring/algorithm/programmers/Question.java
@@ -1,21 +1,94 @@
 package study.spring.algorithm.programmers;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Question {
 
-  public int solution(int[] d, int budget) {
-    int answer = 0;
-    int sum = 0;
-    Arrays.sort(d);
+  Map<String, List<Integer>> map = new HashMap<>();
 
-    for (int num : d) {
-      sum += num;
-      if (sum > budget) break;
-      answer++;
+  public int[] solution(String[] info, String[] query) {
+    int[] answer = new int[query.length];
+
+    for(String str : info) {
+
+      // 중복
+      String[] arr = str.split(" ");
+      int score = Integer.parseInt(arr[arr.length-1]);
+
+      makeKey(arr, 0, "", score);
+    }
+
+    for (List<Integer> list : map.values()) {
+      Collections.sort(list); //
+    }
+
+    for (int i=0; i< query.length; i++) {
+
+      String[] arr = query[i].split(" ");
+      int score = Integer.parseInt(arr[arr.length-1]);
+
+      String key = findKey(arr);
+
+      List<Integer> list = map.getOrDefault(key, new ArrayList<>());
+
+      int idx = binarySearch(list, score); // 다시 한번 봐보기
+
+      if (idx == -1) {
+        idx = list.size();
+      }
+
+      answer[i] = list.size() - idx;
+
     }
 
     return answer;
   }
 
+  public void makeKey(String[] arr, int depth, String key, int score) {
+
+    if (depth == arr.length-1) {
+
+      if (!map.containsKey(key)) {
+        map.put(key, new ArrayList<>());
+      }
+
+      map.get(key).add(score);
+
+      return;
+
+    }
+    makeKey(arr, depth+1, key + arr[depth], score);
+    makeKey(arr, depth+1, key + "-", score);
+  }
+
+  public String findKey(String[] arr) {
+
+    String key = "";
+    for (int i=0; i<arr.length; i+=2) {
+      key += arr[i];
+    }
+
+    return key;
+  }
+
+  public int binarySearch(List<Integer> list, int score) {
+    int low = 0;
+    int high = list.size()-1;
+    int result = -1;
+
+    while(low <= high) {
+
+      int mid = (high + low) / 2;
+      int midScore = list.get(mid);
+
+      if (midScore < score) {
+        low = mid + 1;
+      } else {
+        result = mid; // !!
+        high = mid - 1;
+      }
+    }
+
+    return result;
+  }
 }
--- src/test/java/study/spring/algorithm/programmers/QuestionTest.java
+++ src/test/java/study/spring/algorithm/programmers/QuestionTest.java
@@ -2,6 +2,7 @@ package study.spring.algorithm.programmers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +14,26 @@ class QuestionTest {
   @DisplayName("테스트 코드 검증")
   void test() {
 
-    int[] d = {1, 3, 2, 5, 4};
-    int budget = 9;
+    String[] info = {
+        "java backend junior pizza 150",
+        "python frontend senior chicken 210",
+        "python frontend senior chicken 150",
+        "cpp backend senior pizza 260",
+        "java backend junior chicken 80",
+        "python backend senior chicken 50"
+    };
 
-    int answer = question.solution(d, budget);
+    String[] query = {
+        "java and backend and junior and pizza 100",
+        "python and frontend and senior and chicken 200",
+        "cpp and - and senior and pizza 250",
+        "- and backend and senior and - 150",
+        "- and - and - and chicken 100",
+        "- and - and - and - 150"
+    };
 
-    assertThat(answer).isEqualTo(3);
+    Assertions.assertThat(question.solution(info, query))
+        .isEqualTo(new int[]{1,1,1,1,2,4});
 
   }
 }
